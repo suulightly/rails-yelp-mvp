c35663b5be1e4d1833b9e4d5f72fe19919936247
Liwen S <liwenxsu@gmail.com>
2021-11-15 21:48:43 +0800

    yelp mvp rails

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new; end
+
+  def create
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    { name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      category: %w[chinese italian japanese french belgian].sample }
+  )
+
+  restaurant.save
+end
