7bc61e8e3e878c2ef83f77a57b79ccf84c999885
Liwen S <liwenxsu@gmail.com>
2021-11-15 22:39:39 +0800

    tweaking embedded review form actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
+    @review.save
+    redirect_to restaurant_path(@restaurant)
     # else
     #   render :new
-    end
+    # end
   end
 
   private
